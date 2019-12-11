7eda272d4755d09e1ec34cb7eef91cd59ceb8be5
leothebeagle <a.kosber@gmail.com>
2019-12-11 15:25:18 -0500

    Draw RESTFUL route for random quote

--- DashboardApp-BackEnd-api/config/routes.rb
+++ DashboardApp-BackEnd-api/config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resources :quotes
+  get 'quotes/random', to: 'quotes#random'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
