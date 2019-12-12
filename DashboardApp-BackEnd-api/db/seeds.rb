0a9821a7e747cff777af9d0a08d3940d0bbffbb7
leothebeagle <a.kosber@gmail.com>
2019-12-12 14:16:00 -0500

    Add seed data for quotes

--- DashboardApp-BackEnd-api/db/seeds.rb
+++ DashboardApp-BackEnd-api/db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+quotes = Quote.create([{author: "Khalil Gibran" , content: "" },
+    {author: "Khalil Gibran" , content: "When you enjoy loving your neighbor it ceases to be a virtue." },
+    {author: "Khalil Gibran" , content: "Strange that creatures without backbones have the hardest shells." },
+    {author: "Khalil Gibran" , content: "When night comes and you too are dark, lie down and be dark with a will.
+        And when morning comes and you are still dark stand up and say to the day with a will, I am still dark.
+        It is stupid to play a role with the night and the day.
+        They would both laugh at you.
+        " },
+    { author: "Khalil Gibran" , content: "The most talkative is the least intelligent, and there is hardly a difference between an orator and an auctioneer." },
+    { author: "Khalil Gibran" , content: "A madman is not less a musician than you or myself; only the instrument on which he plays is a little out of tune." },
+    { author: "Khalil Gibran" , content: "How stupid is he who would patch the hatred in his eyes with the smile of his lips." },
+    { author: "Khalil Gibran" , content: "Perhaps a man may commit suicide in self-defense." },
+    { author: "Khalil Gibran" , content: "It is only when you are pursued that you become swift." },
+    { author: "Khalil Gibran" , content: "A great singer is he who sings our silences." },
+    { author: "Khalil Gibran" , content: "When you long for blessings that you may not name, and when you grieve knowing not the cause, then indeed you are growing with all things that grow, and rising toward your greater self." },
+    { author: "Khalil Gibran" , content: "Let there be spaces in your togetherness." },
+    { author: "Khalil Gibran" , content: "Your pain is the breaking of the shell that encloses your understanding." },
+    { author: "Khalil Gibran" , content: "Love one another, but make not a bond of love: Let it rather be a moving sea between the shores of your souls." },
+    { author: "Khalil Gibran" , content: "In the sweetness of friendship let there be laughter, and sharing of pleasures. For in the dew of little things the heart finds its morning and is refreshed." },
+    { author: "Khalil Gibran" , content: "Ever has it been that love knows not its own depth until the hour of separation." },
+    { author: "Khalil Gibran" , content: "Out of suffering have emerged the strongest souls; the most massive characters are seared with scars." }
+    ])
