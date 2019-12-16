10f7cda37e10ece2fd9bfd89eb5fe127dde1f808
leothebeagle <a.kosber@gmail.com>
2019-12-16 09:41:16 -0500

    Add basic event listener and cb on quote button

--- DashboardApp-FrontEnd/src/index.js
+++ DashboardApp-FrontEnd/src/index.js
@@ -1,7 +1,10 @@
-
 class Quote {
     constructor(author, content) {
         this.author = author;
         this.content = content;
     }
 }
+
+const quoteButton = document.querySelector("#rndm-quote-btn");
+
+quoteButton.addEventListener("click", () => console.log("hi, im firing from inside the callback used by the event listener"))
