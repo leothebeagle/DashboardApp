93f3272dbfcb75410bf18a0cfb136aba9bbd590d
leothebeagle <a.kosber@gmail.com>
2019-12-16 17:38:48 -0500

    Complete random quote feature

--- DashboardApp-FrontEnd/src/index.js
+++ DashboardApp-FrontEnd/src/index.js
@@ -6,5 +6,15 @@ class Quote {
 }
 
 const quoteButton = document.querySelector("#rndm-quote-btn");
+const randomQuoteCardContent = document.querySelector(".random-quote-card .quote-card-content")
 
-quoteButton.addEventListener("click", () => console.log("hi, im firing from inside the callback used by the event listener"))
+quoteButton.addEventListener("click", retrieveRandomQuote)
+
+function retrieveRandomQuote() {
+    fetch("http://localhost:3000/quotes/random")
+        .then((response) => response.json())
+        .then(function(json) {
+            let quote = new Quote(json[0].author, json[0].content);
+            randomQuoteCardContent.innerHTML =  `"${quote.content} "` + " - " + quote.author
+        })
+}
