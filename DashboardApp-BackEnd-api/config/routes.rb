b1e021454b3f1c75f4a72709bebde193cab94930
leothebeagle <a.kosber@gmail.com>
2019-12-16 23:28:02 -0500

    Generate resource in Rails for Resource

--- DashboardApp-BackEnd-api/config/routes.rb
+++ DashboardApp-BackEnd-api/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :resources
   resources :events
   get 'quotes/random', to: 'quotes#random'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
