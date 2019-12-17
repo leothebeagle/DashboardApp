6057f32a447ca2f481a552165ebffcdc0412b060
leothebeagle <a.kosber@gmail.com>
2019-12-17 08:20:26 -0500

    Redraw routes using Rails built-in resources

--- DashboardApp-BackEnd-api/config/routes.rb
+++ DashboardApp-BackEnd-api/config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :resources
-  resources :events
+  
   get 'quotes/random', to: 'quotes#random'
+  resources :events, only: [:create, :destroy]
+  resources :resources, only: [:create]
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
