e60750a19399a6d3f34c472771b9c0ab7b192c03
leothebeagle <a.kosber@gmail.com>
2019-12-17 22:41:02 -0500

    Add resource in EventsController create action

--- DashboardApp-BackEnd-api/app/controllers/events_controller.rb
+++ DashboardApp-BackEnd-api/app/controllers/events_controller.rb
@@ -1,6 +1,7 @@
 class EventsController < ApplicationController
     def create 
         @event = Event.create(event_title:params["event-title"], suggested_time:params["suggested-time"])
+        @event.resources.create(url:params["url"], resource_name:params["resource-name"], resource_description:params["resource-description"])
         render json: @event
     end 
 
@@ -12,3 +13,6 @@ end
 # {"event-title"=>"code", "suggested-time"=>"60", "url"=>"leo.com", "resource-name"=>"leos diary", 
 #     "resource-description"=>"a journal of beagleness", "controller"=>"events", 
 #     "action"=>"create", "event"=>{}}
+
+# render :json => @programs, :include => {:insurer => {:only => :name}}, :except => [:created_at, :updated_at]
+
