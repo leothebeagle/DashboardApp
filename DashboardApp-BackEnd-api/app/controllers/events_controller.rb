a2bde78f67d1425549434758ae2e87c86f3ce853
leothebeagle <a.kosber@gmail.com>
2019-12-17 22:23:22 -0500

    Complete create action in EventsController

--- DashboardApp-BackEnd-api/app/controllers/events_controller.rb
+++ DashboardApp-BackEnd-api/app/controllers/events_controller.rb
@@ -1,8 +1,14 @@
 class EventsController < ApplicationController
     def create 
-        raise params.inspect
+        @event = Event.create(event_title:params["event-title"], suggested_time:params["suggested-time"])
+        render json: @event
     end 
 
     def destroy 
     end 
 end
+
+
+# {"event-title"=>"code", "suggested-time"=>"60", "url"=>"leo.com", "resource-name"=>"leos diary", 
+#     "resource-description"=>"a journal of beagleness", "controller"=>"events", 
+#     "action"=>"create", "event"=>{}}
