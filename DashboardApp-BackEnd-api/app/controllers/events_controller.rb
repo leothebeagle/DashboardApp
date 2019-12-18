f58e666e1dcf76924cc4891b88a20109f7a1e15d
leothebeagle <a.kosber@gmail.com>
2019-12-17 22:46:54 -0500

    Return associated resources in event JSON response

--- DashboardApp-BackEnd-api/app/controllers/events_controller.rb
+++ DashboardApp-BackEnd-api/app/controllers/events_controller.rb
@@ -2,7 +2,7 @@ class EventsController < ApplicationController
     def create 
         @event = Event.create(event_title:params["event-title"], suggested_time:params["suggested-time"])
         @event.resources.create(url:params["url"], resource_name:params["resource-name"], resource_description:params["resource-description"])
-        render json: @event
+        render json: @event, :include => [:resources]
     end 
 
     def destroy 
@@ -16,3 +16,4 @@ end
 
 # render :json => @programs, :include => {:insurer => {:only => :name}}, :except => [:created_at, :updated_at]
 
+# https://learn.co/tracks/full-stack-web-development-v8/module-14-front-end-web-programming-in-javascript/section-6-rails-as-an-api/rendering-related-object-data-in-json
