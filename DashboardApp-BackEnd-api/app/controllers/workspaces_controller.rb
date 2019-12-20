bfdeefe16a7f734431a09f9d7c0553caa75ce805
leothebeagle <a.kosber@gmail.com>
2019-12-20 16:10:06 -0500

    Render newly created workspace object as JSON

--- DashboardApp-BackEnd-api/app/controllers/workspaces_controller.rb
+++ DashboardApp-BackEnd-api/app/controllers/workspaces_controller.rb
@@ -1,12 +1,12 @@
 class WorkspacesController < ApplicationController
     def create 
-        binding.pry
+    
         # what data do we need from params? 
         # you can access the name in two ways:  params["name"]
         # or params["workspace"]["name"]
         # ill go for the second option. more descriptive and it promises to be more organized. 
         new_workspace = Workspace.create(name: params["workspace"]["name"])
-        # render json: new_workspace 
+        render json: new_workspace 
         # maybe you havent made this connection yet, but the response is also sent back 
         # through the same route that the post request arrived through. so yes the url is serving back a response. 
     end
