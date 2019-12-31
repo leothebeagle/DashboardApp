3e6d4047ab00f6c97d66a3342f4712aec93d484b
leothebeagle <a.kosber@gmail.com>
2019-12-31 17:34:30 -0500

    Remove routes for resources

--- DashboardApp-BackEnd-api/config/routes.rb
+++ DashboardApp-BackEnd-api/config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   resources :workspaces
   get 'quotes/random', to: 'quotes#random'
   resources :events, only: [:create, :destroy]
-  resources :resources, only: [:create, :destroy]
+  
+
 
 
 
