aca992ff834a49a5a9d5042afb4cc48dc99ecfae
leothebeagle <a.kosber@gmail.com>
2020-01-26 13:42:21 -0500

    Clean up create action in workspaces controller

--- DashboardApp-BackEnd-api/app/controllers/workspaces_controller.rb
+++ DashboardApp-BackEnd-api/app/controllers/workspaces_controller.rb
@@ -1,26 +1,20 @@
 class WorkspacesController < ApplicationController
     
-    def show 
-        workspace = Workspace.first
+    # def show 
+    #     workspace = Workspace.first
 
-        render :json => workspace, :include => :events, :except => [:created_at, :updated_at]
+    #     render :json => workspace, :include => :events, :except => [:created_at, :updated_at]
+    # end
+
+    def create
+        new_workspace = Workspace.create(name: params[:workspace][:name])
+        render json: new_workspace
     end
 
-    def create 
-        # what data do we need from params? 
-        # you can access the name in two ways:  params["name"]
-        # or params["workspace"]["name"]
-        # ill go for the second option. more descriptive and it promises to be more organized. 
-        new_workspace = Workspace.create(name: params["workspace"]["name"])
-        render json: new_workspace 
-        # maybe you havent made this connection yet, but the response is also sent back 
-        # through the same route that the post request arrived through. so yes the url is serving back a response. 
-    end
-
-    def index 
-        workspaces = Workspace.all 
-        render :json => workspaces, :include => :events, :except => [:created_at, :updated_at]
+    # def index 
+    #     workspaces = Workspace.all 
+    #     render :json => workspaces, :include => :events, :except => [:created_at, :updated_at]
         
-    end
+    # end
 end
 
