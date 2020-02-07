2b881a05611059a2c2b94e507dad79469401ba36
leothebeagle <a.kosber@gmail.com>
2020-02-07 13:18:30 -0500

    Complete create action in EventsController

--- DashboardApp-BackEnd-api/app/controllers/events_controller.rb
+++ DashboardApp-BackEnd-api/app/controllers/events_controller.rb
@@ -1,12 +1,19 @@
 class EventsController < ApplicationController
-    def create 
-        @event = Event.create(event_title:params["event-title"], suggested_time:params["suggested-time"])
-        @event.resources.create(url:params["url"], resource_name:params["resource-name"], resource_description:params["resource-description"])
-        render json: @event, :include => [:resources]
-    end 
+    # def create 
+    #     @event = Event.create(event_title:params["event-title"], suggested_time:params["suggested-time"])
+    #     @event.resources.create(url:params["url"], resource_name:params["resource-name"], resource_description:params["resource-description"])
+    #     render json: @event, :include => [:resources]
+    # end 
 
-    def destroy 
-    end 
+    # def destroy 
+    # end 
+    def create
+        workspace = Workspace.find(params[:workspace][:id])
+        new_event = workspace.events.create(event_title: params[:event][:name], suggested_time: params[:event][:time])
+        render json: new_event
+        # params received look like:
+        # "event"=>{"name"=>"Fry Onions", "time"=>"30"}, "workspace"=>{"id"=>"1"}
+    end
 end
 
 
